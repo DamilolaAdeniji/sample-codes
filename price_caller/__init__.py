f2500831d17107739c5eff56c41ad81a109733f7
Damilola <oluwadamilola.adeniji@outlook.com>
2024-04-11 20:21:50 +0100

    inital commit

---
+++ price_caller/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import pandas as pd
+import azure.functions as func
+import json
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    accountName = os.environ['ACCOUNT_NAME']
+    containerName = os.environ['containername']
+    filename = os.environ['filename']
+
+
+#    logging.info('Python HTTP trigger function processed a request.')
+    df = pd.read_excel(f'https://{accountName}.blob.core.windows.net/{containerName}/{filename}')
+    df = df[df.columns[:2]]
+    latest_price = df[df.DATE == df.DATE.max()].iloc[0][1]
+    df = df[df.DATE < df.DATE.max()]
+    previous_price = df[df.DATE == df.DATE.max()].iloc[0][1]
+    performance = round((latest_price/previous_price - 1) * 100,2)
+
+    result = {"latest_value": latest_price,"previous_value":previous_price,"performance":f'{performance}%'}
+    
+    return func.HttpResponse(json.dumps(result), mimetype="application/json")
+ #   return func.HttpResponse('{'+"'"+'price'+"'"+':'"'"+f"{latest_price}"+"'"+'}', mimetype="text/plain")
+
+   
---
+++ price_caller/readme.md
@@ -0,0 +1,2 @@
+# What this does
+## This api returns the latest price of gold, the previous price and the perfromance (% change)
